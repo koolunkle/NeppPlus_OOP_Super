4df006dd940ace1c2b1da2a732fb089c538818b8
koolunkle <gugi329@daum.net>
2021-11-15 23:58:40 +0900

    Child 클래스 작성 -> Super 메소드 활용

--- src/Codes/Child.java
+++ src/Codes/Child.java
@@ -7,4 +7,17 @@ public class Child extends Parent {
 		System.out.println("자녀의 테스트 메소드");
 	}
 
+	void superTestMethod() {
+
+		super.testMethod();
+
+	}
+
+	public Child() {
+		super();
+
+		System.out.println("자녀의 생성자 -> 부모의 기본() 생성자 이용");
+
+	}
+
 }
